8ba15b59f5585064ecd13f2465033d15ec76c00f
Adrien-Ba <45963338+Adrien-Ba@users.noreply.github.com>
2019-05-27 11:18:41 +0200

    Ajout de la librairie

--- src/gz/utils/main/Main.java
+++ src/gz/utils/main/Main.java
@@ -1,9 +1,13 @@
 package gz.utils.main;
 
+import javax.transaction.TransactionRequiredException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("T'as plus qu'a coder ici");
+		
+		new TransactionRequiredException();
 
 	}
 
