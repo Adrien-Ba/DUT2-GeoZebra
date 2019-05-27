bc0b126b39b0de67268d14acfeb217b01b14cf70
theteksa <aymeric03d@gmail.com>
2019-05-27 16:04:34 +0200

    mise en place fxml

--- src/gz/utils/main/Main.java
+++ src/gz/utils/main/Main.java
@@ -1,14 +1,26 @@
 package gz.utils.main;
 
-import javax.transaction.TransactionRequiredException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application{
 
-	public static void main(String[] args) {
-		System.out.println("T'as plus qu'a coder ici");
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("rendu.fxml"));
+		Parent root = loader.load();
 		
-		new TransactionRequiredException();
-
+		Scene s = new Scene(root);
+		primaryStage.setScene(s);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		Application.launch(args);
 	}
 
 }
