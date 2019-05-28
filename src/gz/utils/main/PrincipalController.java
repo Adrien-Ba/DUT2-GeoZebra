49b85e264ecfc148883f431b291b5da880f26a8e
theteksa <aymeric03d@gmail.com>
2019-05-28 15:25:30 +0200

    contrller

---
+++ src/gz/utils/main/PrincipalController.java
@@ -0,0 +1,112 @@
+package gz.utils.main;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+public class PrincipalController {
+
+    @FXML
+    private Button btnReculer;
+
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Button btnAvancer;
+
+    @FXML
+    private VBox vBoxHistorique;
+
+    @FXML
+    private Pane panePrincipal;
+
+    @FXML
+    private TextField txtFieldNomTranslation;
+
+    @FXML
+    private TextField txtFieldXTranslation;
+
+    @FXML
+    private TextField txtFieldYTranslation;
+
+    @FXML
+    private Label lblErreurTranslation;
+
+    @FXML
+    private Button btnValiderTranslation;
+
+    @FXML
+    private TextField txtFieldNomRptation;
+
+    @FXML
+    private TextField txtFieldXRotation;
+
+    @FXML
+    private TextField txtFieldYRotation;
+
+    @FXML
+    private TextField txtFieldDegresRotation;
+
+    @FXML
+    private Label lblErreurRotation;
+
+    @FXML
+    private Button btnValiderRotation;
+
+    @FXML
+    private TextField txtFieldNomHomothetie;
+
+    @FXML
+    private TextField txtFieldXHomothetie;
+
+    @FXML
+    private TextField txtFieldYHomothetie;
+
+    @FXML
+    private TextField txtFieldRapportHomothetie;
+
+    @FXML
+    private Label lblErreurHomothetie;
+
+    @FXML
+    private Button btnValiderHomothetie;
+
+    @FXML
+    private Label lblInspector;
+
+    @FXML
+    void btnAvancerListener(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnPlayListener(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnReculerListener(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnValiderHomothetieListener(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnValiderRotationListener(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnValiderTranslationListener(MouseEvent event) {
+
+    }
+
+}
